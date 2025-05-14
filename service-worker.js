2c00053fc8bc5936d83932f5211f7ec29ad14215
no882 <nomanahmad4181@gmail.com>
2025-05-14 13:00:39 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = "jump-hero-cache-v1";
+const assets = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "jump.mp3",
+  "icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
